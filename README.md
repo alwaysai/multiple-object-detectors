c396f62af26cf4d39722125b3360f1514df1ef7a
Lila Mullany <lilamullany@gmail.com>
2020-02-12 14:27:44 -0800

    updated README

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This app enables two alwaysAI models to be used simultaneously: one to detect facial objects, and a second to classify the detected faces in terms of age ranges. Each model's output is displayed in its own video frame.
+This app utilizes two object detection models, with the option of adding additional detection models. This may be helpful for including models that comprise very different libraries. In this example, one model detects numerous objects of small to medium size, and the other has a more limited library but detects some larger objects, such as airplanes, trains, and sofas. As there is some overlap between models, for instance they both detect people, you can compare the prediction confidences. The different model's output appears in separate video frames.
+
 
 To run this app, you will need an alwaysAI account. Please register at https://alwaysai.co/auth?register=true
 
@@ -9,10 +10,8 @@ Easy start up guides can be found following registration. Please see the docs pa
 If you are having trouble connecting to your edge device, use the CLI configure command to reset the device. Please see the following page for more details: https://alwaysai.co/docs/reference/cli_commands.html
 
 
-The age classifier used is the 'alwaysai/agenet' model, and more details can be found at https://alwaysai.co/model-catalog?model=alwaysai/agenet
-
-
-The facial detection used is the 'alwaysai/res10_300x300_ssd_iter_140000' model, and more details can be found at https://alwaysai.co/model-catalog?model=alwaysai/res10_300x300_ssd_iter_140000
-
-
 You can alter the code to used different detection and classification models: https://alwaysai.co/docs/application_development/changing_the_model.html
+
+
+Additional notes:
+For convenience, there is a variable called 'colors' included in the code. Should you alter 'models' to include more than two detection models, also include a new list of tuples to be used as the color to mark up the detected objects in the new model. In the included code, each 'list' contains only one tuple, so all detection boxes for a given model will appear with that color.
--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import time
 import edgeiq
+import numpy
 """
-Simultaneously use object detection to detect human faces and classification to classify
-the detected faces in terms of age groups, and output results to
-shared output stream.
+Simultaneously utilize two object detection models and present the results to
+a single output stream.
 
-To change the computer vision models, follow this guide:
+The models used in this app are ssd_inception_v2_coco_2018_01_28, which can detect
+numerous inanimate objects, such as bikes, utensils, animals, etc., and the
+the mobilenet_ssd, which is a smaller library but detects some larger objects that
+ssd_inception_v2_coco_2018_01_28 does not, such as a sofa, a train, or an airplane.
+
+To change the computer vision model, follow this guide:
 https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
 
 To change the engine and accelerator, follow this guide:
@@ -15,30 +20,37 @@ https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_a
 
 def main():
 
-    # first make a detector to detect facial objects
-    facial_detector = edgeiq.ObjectDetection(
-            "alwaysai/res10_300x300_ssd_iter_140000")
-    facial_detector.load(engine=edgeiq.Engine.DNN)
+    # if you would like to test an additional model, add one to the list below:
+    models = ["alwaysai/mobilenet_ssd", "alwaysai/ssd_inception_v2_coco_2018_01_28"]
 
-    # then make a classifier to classify the age of the image
-    classifier = edgeiq.Classification("alwaysai/agenet")
-    classifier.load(engine=edgeiq.Engine.DNN)
+    # if you've added a model, add a new color in as a list of tuples in BGR format
+    # to make visualization easier (e.g. [(B, G, R)]).
+    colors = [[(66, 68, 179)], [(50, 227, 62)]]
 
-    # descriptions printed to console
-    print("Engine: {}".format(facial_detector.engine))
-    print("Accelerator: {}\n".format(facial_detector.accelerator))
-    print("Model:\n{}\n".format(facial_detector.model_id))
+    detectors = []
 
-    print("Engine: {}".format(classifier.engine))
-    print("Accelerator: {}\n".format(classifier.accelerator))
-    print("Model:\n{}\n".format(classifier.model_id))
+    # load all the models (creates a new object detector for each model)
+    for model in models:
+
+        # start up a first object detection model
+        obj_detect = edgeiq.ObjectDetection(model)
+        obj_detect.load(engine=edgeiq.Engine.DNN)
+
+        # track the generated object detection items by storing them in detectors
+        detectors.append(obj_detect)
+
+        # print the details of each model to the console
+        print("Model:\n{}\n".format(obj_detect.model_id))
+        print("Engine: {}".format(obj_detect.engine))
+        print("Accelerator: {}\n".format(obj_detect.accelerator))
+        print("Labels:\n{}\n".format(obj_detect.labels))
 
     fps = edgeiq.FPS()
 
     try:
         with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
                 edgeiq.Streamer() as streamer:
-
+            
             # Allow Webcam to warm up
             time.sleep(2.0)
             fps.start()
@@ -47,37 +59,31 @@ def main():
             while True:
                 frame = video_stream.read()
 
-                # detect human faces
-                results = facial_detector.detect_objects(
+                text = [""]
+
+                # gather data from the all the detectors 
+                for i in range(0, len(detectors)):
+                    results = detectors[i].detect_objects(
                         frame, confidence_level=.5)
-                frame = edgeiq.markup_image(
-                        frame, results.predictions, show_labels=False)  
+                    object_frame = edgeiq.markup_image(
+                        frame, results.predictions, show_labels=False, colors=colors[i])
 
-                # generate labels to display the facial detections on the streamer output
-                text = ["Model: {}".format(facial_detector.model_id)]
-                text.append(
-                        "Inference time: {:1.3f} s".format(results.duration))
-                text.append("Objects:")
+                    # for the first frame, overwrite the input feed
+                    if i == 0:
+                        display_frame = object_frame
+                    else:
 
-                # append each predication to the text output
-                for prediction in results.predictions:
-                    text.append("{}: {:2.2f}%".format(
-                        prediction.label, prediction.confidence * 100))
+                        # otherwise, append the new marked-up frame to the previous one
+                        display_frame = numpy.concatenate((object_frame, display_frame))
 
-                # attempt to classify the image in terms of age
-                age_results = classifier.classify_image(frame)
-
-                # if there are predictions for the age classification,
-                # generate these labels for the output stream
-                if age_results.predictions:
-                    text.append("Label: {}, {:.2f}".format(
-                        age_results.predictions[0].label,
-                        age_results.predictions[0].confidence))
-                else:
-                    text.append("No age predication")    
+                    # append each prediction
+                    for prediction in results.predictions:
+                        text.append("Model {} detects {}: {:2.2f}%".format(detectors[i].model_id,
+                            prediction.label, prediction.confidence * 100))
                 
-                # send the image frame and the predictions to the output stream
-                streamer.send_data(frame, text)
+                # send the image frame and the predictions for both 
+                # prediction models to the output stream
+                streamer.send_data(display_frame, text)
 
                 fps.update()
 
