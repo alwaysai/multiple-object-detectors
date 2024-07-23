49d1103c88c0633c812b1d0ec67defe4a4cd8f7c
Eric VanBuhler <eric@alwaysai.co>
2024-07-23 10:27:58 -0700

    Update model links

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ the mobilenet_ssd, which is a smaller library but detects some larger objects th
 ssd_inception_v2_coco_2018_01_28 does not, such as a sofa, a train, or an airplane.
 
 To change the computer vision model, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
+https://docs.alwaysai.co/application_development/application_configuration.html#change-the-computer-vision-model
 
 To change the engine and accelerator, follow this guide:
 https://docs.alwaysai.co/application_development/application_configuration.html#change-the-engine-and-accelerator
